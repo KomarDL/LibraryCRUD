41fd6f516c8d40e3c5d98a1aca7901283de4fee4
KomarDL <deagl_Dim@mail.ru>
2019-12-03 12:17:51 +0300

    Final

--- src/crud/Main.java
+++ src/crud/Main.java
@@ -1,13 +1,12 @@
 package crud;
 
 import crud.bean.Client;
+import crud.view.ViewFactory;
 
 import java.util.*;
 
 public class Main {
     public static void main(String args[]) {
-        String version = System.getProperty("java.version");
-        System.out.println(version);
-
+        ViewFactory.getInstance().getLibraryView().start();
     }
 }
